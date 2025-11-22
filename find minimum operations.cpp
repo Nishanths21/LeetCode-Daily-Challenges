9448572a62a982497b9c88397aff486441481648
Nishanths21 <nishanths1441@gmail.com>
2025-11-22 23:39:34 +0530

    21.11.25

---
+++ find minimum operations.cpp
@@ -0,0 +1,9 @@
+ #include<bits/stdc++.h>
+ using namespace std;
+ class Solution {
+public:
+    int minimumOperations(vector<int>& nums) {
+        return count_if(nums.begin(), nums.end(), [&](int x){
+            return x%3!=0; });
+    }
+};
