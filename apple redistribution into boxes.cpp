6cf8510120c83aa27cc599aab1a874406757ecff
Nishanths21 <nishanths1441@gmail.com>
2025-12-24 14:53:49 +0530

    24.12.25

---
+++ apple redistribution into boxes.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int minimumBoxes(vector<int>& apple, vector<int>& cap) {
+        int sum = accumulate(apple.begin(), apple.end(), 0);
+        sort(cap.begin(), cap.end(), greater<int>());
+
+        int res = 0;
+        while (sum > 0)
+            sum -= cap[res++];
+
+        return res;
+    }
+};
