799964eca3241222fd74e1edae3e36b4296b8275
Nishanths21 <nishanths1441@gmail.com>
2025-12-25 20:10:58 +0530

    25.12.25

---
+++ maximize the happiness of selected children.cpp
@@ -0,0 +1,19 @@
+#include<bts/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    long long maximumHappinessSum(vector<int>& christmasJoy, int gifts) {
+        // Santa gives gifts to the happiest children first
+        sort(christmasJoy.begin(), christmasJoy.end(), greater<int>());
+        
+        long long totalJoy = 0;
+        for (int turn = 0; turn < gifts; ++turn) {
+            int currentJoy = christmasJoy[turn] - turn;
+            if (currentJoy <= 0)
+                break;
+            totalJoy += currentJoy;
+        }
+        
+        return totalJoy;
+    }
+};
