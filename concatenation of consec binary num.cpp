c781443b1820a3d7de9b70847d566617bbbf2ce0
Nishanths21 <nishanths1441@gmail.com>
2026-02-28 21:49:29 +0530

    28.2.26

---
+++ concatenation of consec binary num.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    static int concatenatedBinary(int n) {
+        constexpr int mod=1e9+7;
+        uint64_t ans=0;
+        for(unsigned x=1; x<=n; x++){
+            const int bw=32-countl_zero(x);
+            ans=(ans<<bw)+x;
+            if (ans>=mod) ans%=mod;
+        }
+        return ans;
+    }
+};
