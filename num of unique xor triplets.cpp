2547532883289950ab9b7f318b88455602a600c1
Nishanths21 <nishanths1441@gmail.com>
2026-07-23 22:36:54 +0530

    23.7.26

---
+++ num of unique xor triplets.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int uniqueXorTriplets(vector<int>& nums) {
+        size_t n = nums.size();
+        return 1 << (bit_width(n) - 3 / (n + 1));
+    }
+};
