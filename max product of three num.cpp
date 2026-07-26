85d70c26ff5363ae0596cae0b5531ec5a160092a
Nishanths21 <nishanths1441@gmail.com>
2026-07-26 21:46:03 +0530

    26.7.26

---
+++ max product of three num.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int maximumProduct(vector<int>& A) {
+        ranges::sort(A);
+        return max(
+            A.back() * A[A.size() - 2] * A[A.size() - 3],
+            A.back() * A.front() * A[1]
+        );
+    }
+};
