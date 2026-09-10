925c51d2b6f6669be99a47bf42e73a500bd2669e
Nishanths21 <nishanths1441@gmail.com>
2026-09-10 21:05:28 +0530

    10.9.26

---
+++ count nodes equal to avg of subtree.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+    int ans = 0;
+    pair<int,int> dfs(TreeNode* node) {
+        if (!node) return {0, 0};
+        auto [ls, lc] = dfs(node->left);
+        auto [rs, rc] = dfs(node->right);
+        int sum = ls + rs + node->val;
+        int cnt = lc + rc + 1;
+        if (sum / cnt == node->val) ans++;
+        return {sum, cnt};
+    }
+public:
+    int averageOfSubtree(TreeNode* root) {
+        dfs(root);
+        return ans;
+    }
+};
